022951344b083bfaf6893a8db8892eeb59ae4f33
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-04-04 15:09:46 -0600

    Created ToolsController/ToolsService -  getAll, getById, create, update, and delete Tools methods

--- server/src/controllers/ToolsController.js
+++ server/src/controllers/ToolsController.js
@@ -1,3 +1,5 @@
+
+import { Auth0Provider } from "@bcwdev/auth0provider"
 import { toolsService } from "../services/ToolsService.js"
 import BaseController from "../utils/BaseController.js"
 
@@ -7,8 +9,12 @@ export class ToolsController extends BaseController {
     this.router
       .get('', this.getAllTools)
       .get('/:toolId', this.getToolById)
+      // .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createTool)
+      .put('/:toolId', this.updateTool)
+      .delete('/:toolId', this.deleteTool)
   }
+
   /**
    * @param {import("express").Request} request
    * @param {import("express").Response} response
@@ -41,10 +47,41 @@ export class ToolsController extends BaseController {
    * @param {import("express").Response} response
    * @param {import("express").NextFunction} next
    */
+  //NOTE - remember to implement auth0
   async createTool(request, response, next) {
     try {
       const toolData = request.body
       const tool = await toolsService.createTool(toolData)
+      response.send(tool)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
+  //NOTE - remember to implement auth0
+  async updateTool(request, response, next) {
+    try {
+      const toolId = request.params.toolId
+      const toolData = request.body
+      const tool = await toolsService.updateTool(toolId, toolData)
+      response.send(tool)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
+  async deleteTool(request, response, next) {
+    try {
+      const toolId = request.params.toolId
+      const tool = await toolsService.deleteTool(toolId)
       response.send(tool)
     } catch (error) {
       next(error)
--- server/src/services/ToolsService.js
+++ server/src/services/ToolsService.js
@@ -11,9 +11,28 @@ class ToolsService {
     return tool
   }
   async createTool(toolData) {
+    //NOTE - remember to implement auth0 check
     const tool = await dbContext.Tools.create(toolData)
     return tool
   }
+  async updateTool(toolId, toolData) {
+    //NOTE - remember to implement auth0 check
+    const toolToUpdate = await dbContext.Tools.findById(toolId)
 
+    toolToUpdate.name = toolData.name ?? toolToUpdate.name
+    toolToUpdate.picture = toolData.picture ?? toolToUpdate.picture
+    toolToUpdate.description = toolData.description ?? toolToUpdate.description
+    toolToUpdate.locationId = toolData.locationId ?? toolToUpdate.locationId
+
+    toolToUpdate.save()
+    return toolToUpdate
+  }
+  async deleteTool(toolId) {
+    const tool = await dbContext.Tools.findById(toolId)
+    const toolName = tool.name
+    await tool.deleteOne()
+    return `${toolName} has been deleted. Have a nice day, murderer.`
+  }
 }
+
 export const toolsService = new ToolsService
