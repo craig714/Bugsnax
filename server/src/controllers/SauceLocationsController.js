2f3d4ee1e8e8866b33bd98d466c4f20bb5622408
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-04-07 09:41:47 -0600

    Created getAllSauceLocations()

--- server/src/controllers/SauceLocationsController.js
+++ server/src/controllers/SauceLocationsController.js
@@ -7,8 +7,23 @@ export class SauceLocationsController extends BaseController {
   constructor() {
     super('api/sauceLocations')
     this.router
+      .get('', this.getAllSauceLocations)
       // .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createSauceLocations)
+  }
+
+  /**
+   * @param {import("express").Request} request
+   * @param {import("express").Response} response
+   * @param {import("express").NextFunction} next
+   */
+  async getAllSauceLocations(request, response, next) {
+    try {
+      const sauceLocations = await sauceLocationsService.getAllSauceLocations()
+      response.send(sauceLocations)
+    } catch (error) {
+
+    }
   }
   /**
    * @param {import("express").Request} request
--- server/src/services/SauceLocationsService.js
+++ server/src/services/SauceLocationsService.js
@@ -3,6 +3,10 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class SauceLocationsService {
+  async getAllSauceLocations() {
+    const sauceLocations = await dbContext.SauceLocations.find()
+    return sauceLocations
+  }
   async createSauceLocations(sauceLocationData) {
     const sauceLocation = await dbContext.SauceLocations.create(sauceLocationData)
     return sauceLocation
