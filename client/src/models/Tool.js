6277734d87763599f6a185c07eaa801ba7ce6e86
Craig Goodman <Craig.7144@gmail.com>
2025-04-07 11:46:46 -0600

    Tool model

---
+++ client/src/models/Tool.js
@@ -0,0 +1,8 @@
+export class Tool {
+  constructor(data) {
+    this.name = data.name
+    this.picture = data.picture
+    this.description = data.description
+    this.locationId = data.locationId
+  }
+}
