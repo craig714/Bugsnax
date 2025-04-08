c4a9b07f6fc1f8fd82c830e45e97406db6832b9a
Craig Goodman <Craig.7144@gmail.com>
2025-04-08 12:34:39 -0600

    bugsnax detail page correctly displayes all information

--- client/src/pages/BugsnaxDetailsPage.vue
+++ client/src/pages/BugsnaxDetailsPage.vue
@@ -45,16 +45,42 @@ async function getBugsnaxById() {
           {{ bugsnax.name }}
         </div>
         <div>
-          <img class="cover-img mt-4" :src="bugsnax.picture" alt="">
+          <img class="cover-img mt-4" :src="bugsnax.picture" :alt="bugsnax.name">
         </div>
         <div class="mt-5 fs-4">
           <p class="fw-bold">Description:</p> <span>{{ bugsnax.description }}</span>
         </div>
         <div class="mt-5 fs-4">
-          <p class="fw-bold">Strategy:</p><span>{{ bugsnax.strategy }}</span>
+          <p class="fw-bold">Strategy:</p> <span>{{ bugsnax.strategy }}</span>
         </div>
         <div class="d-flex justify-content-between mt-5">
-
+          <div>
+            <span class="fw-bold fs-5">Likes:</span>
+            <div v-if="bugsnax.likes && bugsnax.likes.length > 0">
+              <div v-for="like in bugsnax.likes" :key="like.name">
+                {{ like.name }}
+              </div>
+            </div>
+            <p v-else>None</p>
+          </div>
+          <div>
+            <span class="fw-bold fs-5">Dislikes:</span>
+            <div v-if="bugsnax.dislikes && bugsnax.dislikes.length > 0">
+              <div v-for="dislike in bugsnax.dislikes" :key="dislike.name">
+                {{ dislike.name }}
+              </div>
+            </div>
+            <p v-else>None</p>
+          </div>
+          <div>
+            <span class="fw-bold fs-5">Fears:</span>
+            <div v-if="bugsnax.fears && bugsnax.fears.length > 0">
+              <div v-for="fear in bugsnax.fears" :key="fear.name">
+                {{ fear.name }}
+              </div>
+            </div>
+            <p v-else>None</p>
+          </div>
         </div>
       </div>
     </div>
